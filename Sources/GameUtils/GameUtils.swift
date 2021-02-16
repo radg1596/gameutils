3e62d2eb0feddca8727ceca3d5d16cae54220195
francisco.garcia <francisco.garcia@globant.com>
2021-02-16 09:43:35 -0600

    Apply gitignore. Adding Sources

--- Sources/GameUtils/GameUtils.swift
+++ Sources/GameUtils/GameUtils.swift
@@ -1,3 +1,36 @@
+import Foundation
+
 struct GameUtils {
     var text = "Hello, World!"
+}
+
+
+/// Used to store the user player name on the userDefaults, using the default standart.
+public class PlayerNameUserDefaultsStorage {
+
+    // MARK: - PROPERTIES
+    /// The storage key in order to use on user defaults. Default value is: "PlayerNameUserDefaultsStorage"
+    public var storageKey: String
+    /// The current name storaged on user defaults, nil if still not saved anything
+    public var currentUserName: String? {
+        return UserDefaults.standard.string(forKey: storageKey)
+    }
+
+    // MARK: INIT
+    public init() {
+        storageKey = "PlayerNameUserDefaultsStorage"
+    }
+
+    // MARK: - METHODS
+    /// Saves a new user name
+    /// - Parameter userName: The desired name to save
+    public func saveNew(userName: String) {
+        UserDefaults.standard.setValue(userName, forKey: storageKey)
+    }
+
+    /// Clears the name saved on the given user defaults key
+    public func clearName() {
+        UserDefaults.standard.removeObject(forKey: storageKey)
+    }
+
 }
--- Tests/GameUtilsTests/GameUtilsTests.swift
+++ Tests/GameUtilsTests/GameUtilsTests.swift
@@ -13,3 +13,46 @@ final class GameUtilsTests: XCTestCase {
         ("testExample", testExample),
     ]
 }
+
+
+class PlayerNameUserDefaultsStorageTests: XCTestCase {
+
+    // MARK: - PROPERTIES
+    private var sut: PlayerNameUserDefaultsStorage?
+
+    // MARK: - TEST CYCLE LIFE
+    override func tearDown() {
+        super.tearDown()
+        sut?.clearName()
+        sut = nil
+    }
+
+    override func setUp() {
+        super.setUp()
+        sut = PlayerNameUserDefaultsStorage()
+    }
+
+    // MARK: - TESTS
+    /// Tests sucess save a name on user defaults
+    func testsSuccessSaveName() {
+        // GIVEN
+        let pathKeyForTests: String = "pathKeyForTests"
+        let expectedName: String = "Rick"
+        // THEN
+        sut?.storageKey = pathKeyForTests
+        sut?.saveNew(userName: expectedName)
+        XCTAssertNotNil(sut?.currentUserName, "Error, not saving the name on userdefaults")
+    }
+
+    /// Tests success save a name, and retrieve the same value
+    func testsSuccessRetreiveSavedName() {
+        // GIVEN
+        let pathKeyForTests: String = "pathKeyForTests"
+        let expectedName: String = "Rick"
+        // THEN
+        sut?.storageKey = pathKeyForTests
+        sut?.saveNew(userName: expectedName)
+        XCTAssertEqual(sut?.currentUserName, expectedName, "Error, not retreive the expected username")
+    }
+
+}
